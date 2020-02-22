68b25298973af7f79a2e8f324a914e03c3958108
Gus Schultz <matthew.schultz@irtc-hq.com>
2020-02-22 08:44:34 -0600

    basic keyboard input working. images working. TODO: Continue defining content, connect keyboard input to selection of activePage()

--- src/index.js
+++ src/index.js
@@ -3,13 +3,44 @@ import ko from 'knockout';
 import './style.css';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import image1 from "../dist/images/javascript.jpg";
+import image2 from '../dist/images/triangle.png';
+import image3 from '../dist/images/html.png'
+import image4 from '../dist/images/css.png'
 import $ from "jquery";
 import data from '../data/pageData.json';
 
-var Page = function (textArray, image) {
+function imageSelect(imageTag) {
+    var image = null;
+    switch(imageTag){
+        case "image1":
+            image = image1;
+            break;
+        case "image2":
+            image = image2;
+            break;
+        case "image3":
+            image = image3;
+            break;
+        case "image4":
+            image = image4;
+            break;
+    }
+    return image;
+}
+
+var Page = function (title, textArray, image) {
     var self = this;
-    this.text = ko.observableArray(textArray);
+    this.title = ko.observable(title)
+    this.text = ko.observableArray();
+    for (var i = 0; i < textArray.length; i++) {
+        this.text.push(textArray[i]);
+    }
     this.image = ko.observable(image || null);
+    if (this.image()) {
+        var imageElement = $('#top-image');
+        imageElement.attr("src", imageSelect(this.image()));
+    }
     return self;
 }
 
@@ -17,7 +48,7 @@ var jsonData = function (data) {
     var self = this;
     this.pages = ko.observableArray();
     for (var i = 0; i < data.length; i++) {
-        this.pages.push(new Page(data[i].textArray, data[i].image));
+        this.pages.push(new Page(data[i].title, data[i].textArray, data[i].image));
     }
     return self;
 }
@@ -28,5 +59,14 @@ var ViewModel = function() {
     this.jsonData = new jsonData(data);
     this.activePage = ko.observable(this.jsonData.pages()[0]);
 };
+
+document.addEventListener('keydown', function(event) {
+    if(event.keyCode == 37) {
+        alert('Left was pressed');
+    }
+    else if(event.keyCode == 39) {
+        alert('Right was pressed');
+    }
+});
 
 ko.applyBindings(new ViewModel(), document.getElementById('root'));
